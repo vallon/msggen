70e71cf710e087931306c3118594e6db8d0323f9
Justin Vallon <JustinVallon@gmail.com>
2017-02-26 23:28:01 -0500

    Add excl

--- msggen.cc
+++ msggen.cc
@@ -6,7 +6,7 @@ using namespace std;
 
 string GenerateMessage(const string& greeting, const string& target) {
   ostringstream s;
-  s << greeting << ", " << target << "!";
+  s << greeting << ", " << target << "!!";
   return s.str();
 }
 
